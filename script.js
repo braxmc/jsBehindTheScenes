f05db7ec12d627cf763f187d868c197b80ae7a05
Braxton McClellan <thebraxmc@gmail.com>
2021-03-04 00:01:34 -0700

    end of lecture

--- script.js
+++ script.js
@@ -6,7 +6,24 @@ function calcAge(birthYear) {
   const age = 2021 - birthYear;
 
   function printAge() {
-    const output = `${firstName} you are ${age}, born in ${birthYear}`;
+    let output = `${firstName} you are ${age}, born in ${birthYear}`;
+    console.log(output);
+    
+    if(birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true; // function scoped
+      output = 'New output' // reassigning outter scope var
+      const firstName = 'bob'; // found as closest scope 
+      const str = `Oh you are a millenial, ${firstName}`
+      console.log(str)
+
+      function add(a, b) {
+        return a + b;
+      }
+      console.log(add(2, 3));
+
+      console.log(output);
+    }
+    console.log(millenial)
     console.log(output);
   }
   printAge();
